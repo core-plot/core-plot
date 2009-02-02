9b4a8ddada2d563b83ddd8caecd337b9614cd990
dirkjan.krijnders <devnull@localhost>
2009-02-02 14:56:33 +0000

    - Added NSDecimal conversions for int, CGFloat and  double

--- framework/Source/CPUtilities.h
+++ framework/Source/CPUtilities.h
@@ -1,4 +1,11 @@
 
 #import <Foundation/Foundation.h>
 
+
+int NSDecimalIntValue(NSDecimal dec);
 CGFloat NSDecimalFloatValue(NSDecimal decimalNumber);
+double NSDecimalDoubleValue(NSDecimal dec);
+
+NSDecimal NSDecimalFromInt(int i);
+NSDecimal NSDecimalFromFloat(CGFloat f);
+NSDecimal NSDecimalFromDouble(double d);
--- framework/Source/CPUtilities.m
+++ framework/Source/CPUtilities.m
@@ -1,7 +1,32 @@
 
 #import "CPUtilities.h"
 
+int NSDecimalIntValue(NSDecimal dec)
+{
+	return [[NSDecimalNumber decimalNumberWithDecimal:dec] intValue]; 
+}
+
 CGFloat NSDecimalFloatValue(NSDecimal dec)
 {
 	return [[NSDecimalNumber decimalNumberWithDecimal:dec] floatValue]; 
+}
+
+double NSDecimalDoubleValue(NSDecimal dec)
+{
+	return [[NSDecimalNumber decimalNumberWithDecimal:dec] doubleValue]; 
+}
+
+NSDecimal NSDecimalFromInt(int i)
+{
+	return [[NSNumber numberWithInt:i] decimalValue]; 
+}
+
+NSDecimal NSDecimalFromFloat(CGFloat f)
+{
+	return [[NSNumber numberWithFloat:f] decimalValue]; 
+}
+
+NSDecimal NSDecimalFromDouble(double d)
+{
+	return [[NSNumber numberWithDouble:d] decimalValue]; 
 }
