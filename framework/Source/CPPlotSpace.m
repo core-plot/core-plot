f3227693bf754b53aaaa592cfb89f9a201bb29a5
boyfarrell <devnull@localhost>
2009-02-02 13:08:21 +0000

    Proposing the use of CGAffineTransform in CPPlotSpace.

--- framework/Source/CPPlotSpace.m
+++ framework/Source/CPPlotSpace.m
@@ -1,10 +1,38 @@
 
 #import "CPPlotSpace.h"
-
+#import "CPPlotArea.h"
 
 @implementation CPPlotSpace
 
 @synthesize plotArea;
+@synthesize transformToView;
+@synthesize transformToPlot;
+
+- (void) setPlotArea: (CPPlotArea*) newPlotArea
+{
+  plotArea = newPlotArea;
+  
+  //Need to update the transform as it relied on the min and max x,y points
+  transformToView = [self calculateTransformToView];
+  transformToPlot = CGAffineTransformInvert(transformToView);
+}
+
+- (CGAffineTransform) calculateTransformToView
+{
+  //The default transform does nothing
+  return CGAffineTransformIdentity;
+}
+
+- (id) init
+{
+  self = [super init];
+  if (self != nil) {
+    transformToView = CGAffineTransformIdentity;
+    transformToPlot = CGAffineTransformIdentity;
+    plotArea = nil;
+  }
+  return self;
+}
 
 -(void)dealloc
 {
