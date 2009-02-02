bd299937c58fe87c35f081eddf7b2b16ee52bf12
drewmccormack <drewmccormack@mac.com>
2009-02-02 14:30:04 +0000

    Cleaned up code, giving consistent style (please try to stick to the style of the existing code.)
    Added files for exception definitions, and general utility functions.
    
    Rolled back introduction of affine transform.

--- framework/Source/CPGraph.m
+++ framework/Source/CPGraph.m
@@ -8,44 +8,36 @@
 @synthesize axisSet;
 @synthesize plotArea;
 
-// Temporary method just to show something...
-- (void)drawInContext:(CGContextRef)theContext
+#pragma mark Init/Dealloc
+-(id)init
 {
-	NSAttributedString* tempString = [[NSAttributedString alloc] initWithString:@"CPGraph" attributes:nil];
+	self = [super init];
+	if (self != nil) {
+		plots = [[NSMutableArray alloc] init];
+	}
+	return self;
+}
+
+-(void)dealloc
+{
+	self.axisSet = nil;
+	self.plotArea = nil;
+	[plots release];
+	[super dealloc];
+}
+
+#pragma mark Drawing
+-(void)drawInContext:(CGContextRef)theContext
+{
+    // Temporary method just to show something...
+	NSAttributedString *tempString = [[NSAttributedString alloc] initWithString:@"CPGraph" attributes:nil];
 	[tempString drawAtPoint:NSMakePoint(10.f, 10.f)];
 	[tempString release];
+}
 
-};
-
-#pragma mark getters/setters
-- (void) setBounds:(CGRect)rect
-{
-	[plotArea setBounds:rect];
-	[super setBounds:rect];
-};
-
-- (void) setFrame:(CGRect)rect
-{
-	[plotArea setFrame:rect];
-	[super setFrame:rect];
-};
-
-- (void) setPlotArea:(CPPlotArea*)aPlotArea
-{
-	[plotArea removeFromSuperlayer];
-	[plotArea release];
-	[aPlotArea retain];
-	plotArea = aPlotArea;
-	[self addSublayer:plotArea];
-	[plotArea setNeedsDisplayOnBoundsChange:YES];
-//	[plotArea setFrame:[self frame]];
-};
-
-#pragma mark plots array management
-
+#pragma mark Organizing Plots
 -(void)addPlot:(CPPlot *)plot
 {
-//	[plot setFrame:[plotArea frame]];
 	[plot setNeedsDisplayOnBoundsChange:YES];
 	[plotArea addSublayer:plot];	
 	[plots addObject:plot];
@@ -58,7 +50,7 @@
 
 -(void)removePlotAtIndex:(NSUInteger)index 
 {
-	[[plots objectAtIndex:index] removeFromSuperview];
+	[[plots objectAtIndex:index] removeFromSuperlayer];
 	[plots removeObjectAtIndex:index];
 }
 
@@ -81,11 +73,9 @@
 
 -(CPPlot *)plotWithIdentifier:(id <NSCopying>)identifier 
 {
-	for (CPPlot* plot in plots)
-		// Is this comparison correct?
-		if ([plot identifier] == identifier)
-			return plot;
-	
+	for (CPPlot *plot in plots) {
+        if ( [[plot identifier] isEqual:identifier] ) return plot;
+	}
     return nil;
 }
 
@@ -96,23 +86,22 @@
 	[plots replaceObjectAtIndex:index withObject:plot];
 }
 
-#pragma mark init/dealloc
-
-- (id) init
+#pragma mark Accessors
+-(void)setBounds:(CGRect)rect
 {
-	self = [super init];
-	if (self != nil) {
-		plots = [[NSMutableArray alloc] init];
-	}
-	return self;
+	[plotArea setBounds:rect];
+	[super setBounds:rect];
 }
 
-- (void) dealloc
+-(void)setPlotArea:(CPPlotArea *)aPlotArea
 {
-	self.axisSet = nil;
-	self.plotArea = nil;
-	[plots release];
-	[super dealloc];
+    if ( aPlotArea != plotArea ) {
+        [plotArea removeFromSuperlayer];
+        [plotArea release];
+        plotArea = [aPlotArea retain];
+        [self addSublayer:plotArea];
+        [plotArea setNeedsDisplayOnBoundsChange:YES];
+    }
 }
 
 @end
