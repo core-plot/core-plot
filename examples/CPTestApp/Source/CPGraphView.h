52985267290ee17bda2e737e3e210921c71721c2
dirkjan.krijnders <devnull@localhost>
2009-02-06 19:53:23 +0000

    - Updated CPTestApp to changes in framework
    CPTestApp now uses two NSArrayController's in the xib for its data. But NSDecimalNumber in a NSArrayController is far from ideal (the value is not KVO)

--- examples/CPTestApp/Source/CPGraphView.h
+++ examples/CPTestApp/Source/CPGraphView.h
@@ -5,6 +5,10 @@
 @interface CPGraphView : NSView {
 	CPGraph *graphLayer;
 	IBOutlet NSArrayController *dataSource;
+	IBOutlet NSArrayController* xData, *yData;
 }
+
+@property (retain) CPGraph* graphLayer;
+@property (retain) IBOutlet NSArrayController* xData, *yData;
 
 @end
--- examples/CPTestApp/Source/CPGraphView.m
+++ examples/CPTestApp/Source/CPGraphView.m
@@ -4,9 +4,13 @@
 
 @implementation CPGraphView
 
+@synthesize graphLayer;
+@synthesize xData, yData;
+
 -(id)initWithFrame:(NSRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
+		
         // Initialization code here.
     }
     return self;
@@ -14,47 +18,51 @@
 
 - (void) setupPlot
 {
-	CPPlotArea* plotArea = [[CPPlotArea alloc] init];
-	CPPlot* linePlot = [[CPPlot alloc] init];
+	CPScatterPlot* linePlot = [[CPScatterPlot alloc] init];
+	[linePlot bind:@"xValues" toObject:xData withKeyPath:@"arrangedObjects" options:nil];
+	[linePlot bind:@"yValues" toObject:yData withKeyPath:@"arrangedObjects" options:nil];
 	CPCartesianPlotSpace* cartPlotSpace = [[CPCartesianPlotSpace alloc] init];
-//	[cartPlotSpace setScale:CGPointMake(2.f, 1.f)];
-//	[cartPlotSpace setOffset:CGPointMake(0.f, 0.f)];
 	CPPlotRange x, y;
 	x.location = [[[[NSDecimalNumber alloc] initWithInt:1] autorelease] decimalValue];
 	x.length = [[[[NSDecimalNumber alloc] initWithInt:2] autorelease] decimalValue];
 	y.location = [[[[NSDecimalNumber alloc] initWithInt:1] autorelease] decimalValue];
 	y.length = [[[[NSDecimalNumber alloc] initWithInt:2] autorelease] decimalValue];
-//	NSArray* xTicks = [[NSArray alloc] initWithObjects:
-//					   [[[NSDecimalNumber alloc] initWithInt:1] autorelease],
-//					   [[[NSDecimalNumber alloc] initWithFloat:1.5f] autorelease], 
-//					   [[[NSDecimalNumber alloc] initWithInt:2] autorelease], 
-//					   [[[NSDecimalNumber alloc] initWithFloat:2.5f] autorelease], 
-//					   [[[NSDecimalNumber alloc] initWithInt:3] autorelease], nil];
-	
 	[cartPlotSpace setXRange:x];
 	[cartPlotSpace setYRange:y];
-//	[cartPlotSpace setXMajorTickLocations:xTicks];
-//	[cartPlotSpace setYMajorTickLocations:xTicks];
 	[cartPlotSpace setNeedsDisplayOnBoundsChange:YES];
-	
 	[linePlot setPlotSpace:cartPlotSpace];
 	[linePlot setIdentifier:@"Test Plot"];
-	[[plotArea valueForKey:@"plotSpaces"] addObject:cartPlotSpace];
-	[plotArea addSublayer:cartPlotSpace];
-	[graphLayer setPlotArea:plotArea];
+	[graphLayer addPlotSpace:cartPlotSpace];
 	[graphLayer addPlot:linePlot];
-//	cartPlotSpace.majorTickLineStyle.lineColor = CGColorGetConstantColor(kCGColorWhite);
+
+	linePlot.dataLineStyle.lineWidth = 2.f;
+}
+
+- (void) setupData
+{
+	NSDecimalNumber* x1 = [NSDecimalNumber decimalNumberWithString:@"1.3"];
+	NSDecimalNumber* x2 = [NSDecimalNumber decimalNumberWithString:@"1.7"];
+	NSDecimalNumber* x3 = [NSDecimalNumber decimalNumberWithString:@"2.8"];
+	NSDecimalNumber* y1 = [NSDecimalNumber decimalNumberWithString:@"1.3"];
+	NSDecimalNumber* y2 = [NSDecimalNumber decimalNumberWithString:@"2.3"];
+	NSDecimalNumber* y3 = [NSDecimalNumber decimalNumberWithString:@"2"];
+
+	NSArray* x = [NSArray arrayWithObjects:x1,x2,x3,nil];
+	NSArray* y = [NSArray arrayWithObjects:y1,y2,y3,nil];
 	
-};
+	[xData setContent:x];
+	[yData setContent:y];
+}
 
 - (void)awakeFromNib{
-	graphLayer = [[CPGraph layer] retain];
+	[self setGraphLayer:[CPGraph layer]];
 	[self setLayer:graphLayer];
 	[self setWantsLayer:YES];
 	//	[plotLayer setDelegate:plotLayer];
 	graphLayer.frame = NSRectToCGRect(self.frame);
 	[graphLayer setNeedsDisplayInRect:graphLayer.frame];
 	[graphLayer setNeedsDisplayOnBoundsChange:YES];
+	[self setupData];
 	[self setupPlot];
 }
 
