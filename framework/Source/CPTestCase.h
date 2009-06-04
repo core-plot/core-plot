21131f3c40ee10ea1168978d39ceb019d0bffc90
Eric Skroch <eskroch@mac.com>
2009-06-03 23:13:11 -0400

    Added GTMNSObject+BindingUnitTesting.h to CPTestCase.h

--- framework/Source/CPTestCase.h
+++ framework/Source/CPTestCase.h
@@ -1,6 +1,7 @@
 
 #import "GTMSenTestCase.h"
 #import "GTMNSObject+UnitTesting.h"
+#import "GTMNSObject+BindingUnitTesting.h"
 #import "GTMCALayer+UnitTesting.h"
 #import "GTMAppKit+UnitTesting.h"
 
