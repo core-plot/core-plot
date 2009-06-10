a78aafc020229388dc3b6e5fe598156ad74f570b
Eric Skroch <eskroch@mac.com>
2009-06-09 23:53:22 -0400

    Allow CPXYAxis to set the axis defaults when CPXYAxisSet creates the axes

--- framework/Source/CPXYAxisSet.m
+++ framework/Source/CPXYAxisSet.m
@@ -10,16 +10,10 @@
 {
 	if (self = [super initWithFrame:newFrame]) {
 		CPXYAxis *xAxis = [[CPXYAxis alloc] initWithFrame:newFrame];
-		xAxis.majorTickLength = 10.f;
-		xAxis.minorTickLength = 5.f;
-		xAxis.axisLabelOffset = 20.f;
 		xAxis.coordinate = CPCoordinateX;
         xAxis.tickDirection = CPDirectionDown;
 		
 		CPXYAxis *yAxis = [[CPXYAxis alloc] initWithFrame:newFrame];
-		yAxis.majorTickLength = 10.f;
-		yAxis.minorTickLength = 5.f;
-		yAxis.axisLabelOffset = 20.f;
 		yAxis.coordinate = CPCoordinateY;
         yAxis.tickDirection = CPDirectionLeft;
 		
