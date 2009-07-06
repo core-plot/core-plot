c328ce5334ca365e37c030774e8a1ca33a9e26b1
Eric Skroch <eskroch@mac.com>
2009-07-05 23:33:20 -0400

    Added documentation for CPDefinitions.

--- framework/Source/CPDefinitions.m
+++ framework/Source/CPDefinitions.m
@@ -1,12 +1,10 @@
 
 #import "CPDefinitions.h"
 
-NSTimeInterval kCPIndefiniteDuration = -1;
-
-const CGFloat CPDefaultZPositionGraph = 0;
-const CGFloat CPDefaultZPositionPlotArea = 0; 
-const CGFloat CPDefaultZPositionAxisSet = 9;
-const CGFloat CPDefaultZPositionAxis = 0;
-const CGFloat CPDefaultZPositionPlotSpace = 8;
-const CGFloat CPDefaultZPositionPlot = 0;
-const CGFloat CPDefaultZPositionAxisSetOverlay = 1;
+const CGFloat CPDefaultZPositionGraph = 0;			///< Default Z position for CPGraph.
+const CGFloat CPDefaultZPositionPlotArea = 0;		///< Default Z position for CPPlotArea.
+const CGFloat CPDefaultZPositionAxisSet = 9;		///< Default Z position for CPAxisSet.
+const CGFloat CPDefaultZPositionAxis = 0;			///< Default Z position for CPAxis.
+const CGFloat CPDefaultZPositionPlotSpace = 8;		///< Default Z position for CPPlotSpace.
+const CGFloat CPDefaultZPositionPlot = 0;			///< Default Z position for CPPlot.
+const CGFloat CPDefaultZPositionAxisSetOverlay = 1;	///< Default Z position for the CPAxisSet overlay.
