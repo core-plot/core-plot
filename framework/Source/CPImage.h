0716790448891a19b88073fe069cf3ec0478bd62
Eric Skroch <eskroch@mac.com>
2009-07-24 23:42:15 -0400

    Updated documentation for CPImage.

--- framework/Source/CPImage.h
+++ framework/Source/CPImage.h
@@ -11,12 +11,21 @@
 @property (assign) CGImageRef image;
 @property (assign, getter=isTiled) BOOL tiled;
 
+/// @name Factory Methods
+/// @{
 +(CPImage *)imageWithCGImage:(CGImageRef)anImage;
 +(CPImage *)imageForPNGFile:(NSString *)path;
+///	@}
 
+/// @name Initialization
+/// @{
 -(id)initWithCGImage:(CGImageRef)anImage;
 -(id)initForPNGFile:(NSString *)path;
+///	@}
 
+/// @name Drawing
+/// @{
 -(void)drawInRect:(CGRect)rect inContext:(CGContextRef)context;
+///	@}
 
 @end
