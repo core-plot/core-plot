edfb463d1702d480e509e03081d78c0a8a59dfbb
Eric Skroch <eskroch@mac.com>
2009-10-04 22:28:50 -0400

    Updated unit tests to use plot spaces that are not CPLayers. Added state resources for the CPXYPlotSpace tests.

--- framework/Source/CPXYPlotSpaceTests.h
+++ framework/Source/CPXYPlotSpaceTests.h
@@ -1,11 +1,15 @@
 
 #import "CPTestCase.h"
-#import "CPXYPlotSpace.h"
+
+@class CPLayer;
+@class CPXYPlotSpace;
 
 @interface CPXYPlotSpaceTests : CPTestCase {
+    CPLayer *layer;
     CPXYPlotSpace *plotSpace;
 }
 
+@property (retain,readwrite) CPLayer *layer;
 @property (retain,readwrite) CPXYPlotSpace *plotSpace;
 
 @end
