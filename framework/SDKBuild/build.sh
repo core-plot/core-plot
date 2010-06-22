86d88f5f7b1a42519892882c9ba28fabf0f5beb7
Eric Skroch <eskroch@mac.com>
2010-06-21 23:45:11 -0400

    Updated the SDK build script to handle path names that include spaces.

--- framework/SDKBuild/build.sh
+++ framework/SDKBuild/build.sh
@@ -2,23 +2,23 @@
 
 set -e -u
 
-BUILD=${PWD}/../../build
+BUILD="${PWD}/../../build"
 SDK_VERSION=4.0
 
 rm -rf Distribution
 
-XCODEBUILDFLAGS="-parallelizeTargets -configuration Release -target CorePlot-CocoaTouch -project ../CorePlot-CocoaTouch.xcodeproj SYMROOT=${PWD}/../../build"
+XCODEBUILDFLAGS="-parallelizeTargets -configuration Release -target CorePlot-CocoaTouch -project ../CorePlot-CocoaTouch.xcodeproj"
 
 for x_sdk in iphoneos iphonesimulator ; do 
 	BASESDK_DIR=Distribution/CorePlotSDK/${x_sdk}.sdk
-	xcodebuild $XCODEBUILDFLAGS -sdk ${x_sdk}${SDK_VERSION}
-	mkdir -p $BASESDK_DIR/usr/include/CorePlot
-	mkdir -p $BASESDK_DIR/usr/lib
+	xcodebuild $XCODEBUILDFLAGS SYMROOT="$BUILD" -sdk ${x_sdk}${SDK_VERSION}
+	mkdir -p "$BASESDK_DIR/usr/include/CorePlot"
+	mkdir -p "$BASESDK_DIR/usr/lib"
 
-	cp -a $BUILD/Release-$x_sdk/usr/local/include/* $BASESDK_DIR/usr/include/CorePlot
-	cp -a $BUILD/Release-$x_sdk/libCorePlot-CocoaTouch.a $BASESDK_DIR/usr/lib/libCorePlot.a
-	cp $x_sdk-SDKSettings.plist $BASESDK_DIR/SDKSettings.plist
-	sed -f cp.sed $BUILD/Release-$x_sdk/usr/local/include/CorePlot-CocoaTouch.h > $BASESDK_DIR/usr/include/CorePlot/CorePlot.h
+	cp -a "$BUILD/Release-$x_sdk/usr/local/include/"* "$BASESDK_DIR/usr/include/CorePlot"
+	cp -a "$BUILD/Release-$x_sdk/libCorePlot-CocoaTouch.a" "$BASESDK_DIR/usr/lib/libCorePlot.a"
+	cp $x_sdk-SDKSettings.plist "$BASESDK_DIR/SDKSettings.plist"
+	sed -f cp.sed "$BUILD/Release-$x_sdk/usr/local/include/CorePlot-CocoaTouch.h" > "$BASESDK_DIR/usr/include/CorePlot/CorePlot.h"
 done
 
 cd Distribution
