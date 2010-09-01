3801f8b9aa3438c8a42038249380d902345adaea
Eric Skroch <eskroch@mac.com>
2010-08-31 22:20:56 -0400

    Added NSDecimal conversion functions in CPUtilities for all primitive number types supported by NSNumber.

--- framework/Source/CPUtilities.h
+++ framework/Source/CPUtilities.h
@@ -1,33 +1,71 @@
-
 #import <Foundation/Foundation.h>
 #import "CPDefinitions.h"
 
 /// @file
 
-/// @name NSDecimal Utilities
+/// @name Convert NSDecimal to primitive types
 /// @{
+int8_t CPDecimalCharValue(NSDecimal decimalNumber);
+int16_t CPDecimalShortValue(NSDecimal decimalNumber);
+int32_t CPDecimalLongValue(NSDecimal decimalNumber);
+int64_t CPDecimalLongLongValue(NSDecimal decimalNumber);
+int CPDecimalIntValue(NSDecimal decimalNumber);
 NSInteger CPDecimalIntegerValue(NSDecimal decimalNumber);
-float   CPDecimalFloatValue(NSDecimal decimalNumber);
-double  CPDecimalDoubleValue(NSDecimal decimalNumber);
 
+uint8_t CPDecimalUnsignedCharValue(NSDecimal decimalNumber);
+uint16_t CPDecimalUnsignedShortValue(NSDecimal decimalNumber);
+uint32_t CPDecimalUnsignedLongValue(NSDecimal decimalNumber);
+uint64_t CPDecimalUnsignedLongLongValue(NSDecimal decimalNumber);
+unsigned int CPDecimalUnsignedIntValue(NSDecimal decimalNumber);
+NSUInteger CPDecimalUnsignedIntegerValue(NSDecimal decimalNumber);
+
+float CPDecimalFloatValue(NSDecimal decimalNumber);
+double CPDecimalDoubleValue(NSDecimal decimalNumber);
+
+NSString * CPDecimalStringValue(NSDecimal decimalNumber);
+/// @}
+
+/// @name Convert primitive types to NSDecimal
+/// @{
+NSDecimal CPDecimalFromChar(int8_t i);
+NSDecimal CPDecimalFromShort(int16_t i);
+NSDecimal CPDecimalFromLong(int32_t i);
+NSDecimal CPDecimalFromLongLong(int64_t i);
+NSDecimal CPDecimalFromInt(int i);
 NSDecimal CPDecimalFromInteger(NSInteger i);
+
+NSDecimal CPDecimalFromUnsignedChar(uint8_t i);
+NSDecimal CPDecimalFromUnsignedShort(uint16_t i);
+NSDecimal CPDecimalFromUnsignedLong(uint32_t i);
+NSDecimal CPDecimalFromUnsignedLongLong(uint64_t i);
+NSDecimal CPDecimalFromUnsignedInt(unsigned int i);
 NSDecimal CPDecimalFromUnsignedInteger(NSUInteger i);
+
 NSDecimal CPDecimalFromFloat(float f);
 NSDecimal CPDecimalFromDouble(double d);
 
+NSDecimal CPDecimalFromString(NSString *stringRepresentation);
+/// @}
+
+/// @name NSDecimal arithmetic
+/// @{
 NSDecimal CPDecimalAdd(NSDecimal leftOperand, NSDecimal rightOperand);
 NSDecimal CPDecimalSubtract(NSDecimal leftOperand, NSDecimal rightOperand);
 NSDecimal CPDecimalMultiply(NSDecimal leftOperand, NSDecimal rightOperand);
 NSDecimal CPDecimalDivide(NSDecimal numerator, NSDecimal denominator);
+/// @}
 
+/// @name NSDecimal comparison
+/// @{
 BOOL CPDecimalGreaterThan(NSDecimal leftOperand, NSDecimal rightOperand);
 BOOL CPDecimalGreaterThanOrEqualTo(NSDecimal leftOperand, NSDecimal rightOperand);
 BOOL CPDecimalLessThan(NSDecimal leftOperand, NSDecimal rightOperand);
 BOOL CPDecimalLessThanOrEqualTo(NSDecimal leftOperand, NSDecimal rightOperand);
 BOOL CPDecimalEquals(NSDecimal leftOperand, NSDecimal rightOperand);
+/// @}
 
-NSDecimal CPDecimalFromString(NSString *stringRepresentation);
-
+/// @name NSDecimal utilities
+/// @{
 NSDecimal CPDecimalNaN(void);
 /// @}
 
--- framework/Source/CPUtilities.m
+++ framework/Source/CPUtilities.m
@@ -1,17 +1,125 @@
-
 #import "CPUtilities.h"
 
-#pragma mark -
-#pragma mark Decimal Numbers
+#pragma mark Convert NSDecimal to primitive types
 
 /**
- *	@brief Converts an NSDecimal value to a NSInteger.
+ *	@brief Converts an NSDecimal value to an 8-bit integer.
+ *	@param decimalNumber The NSDecimal value.
+ *	@return The converted value.
+ **/
+int8_t CPDecimalCharValue(NSDecimal decimalNumber)
+{
+	return (int8_t)[[NSDecimalNumber decimalNumberWithDecimal:decimalNumber] charValue]; 
+}
+
+/**
+ *	@brief Converts an NSDecimal value to a 16-bit integer.
+ *	@param decimalNumber The NSDecimal value.
+ *	@return The converted value.
+ **/
+int16_t CPDecimalShortValue(NSDecimal decimalNumber)
+{
+	return (int16_t)[[NSDecimalNumber decimalNumberWithDecimal:decimalNumber] shortValue]; 
+}
+
+/**
+ *	@brief Converts an NSDecimal value to a 32-bit integer.
+ *	@param decimalNumber The NSDecimal value.
+ *	@return The converted value.
+ **/
+int32_t CPDecimalLongValue(NSDecimal decimalNumber)
+{
+	return (int32_t)[[NSDecimalNumber decimalNumberWithDecimal:decimalNumber] longValue]; 
+}
+
+/**
+ *	@brief Converts an NSDecimal value to a 64-bit integer.
+ *	@param decimalNumber The NSDecimal value.
+ *	@return The converted value.
+ **/
+int64_t CPDecimalLongLongValue(NSDecimal decimalNumber)
+{
+	return (int64_t)[[NSDecimalNumber decimalNumberWithDecimal:decimalNumber] longLongValue]; 
+}
+
+/**
+ *	@brief Converts an NSDecimal value to an int.
+ *	@param decimalNumber The NSDecimal value.
+ *	@return The converted value.
+ **/
+int CPDecimalIntValue(NSDecimal decimalNumber)
+{
+	return (int)[[NSDecimalNumber decimalNumberWithDecimal:decimalNumber] intValue]; 
+}
+
+/**
+ *	@brief Converts an NSDecimal value to an NSInteger.
  *	@param decimalNumber The NSDecimal value.
  *	@return The converted value.
  **/
 NSInteger CPDecimalIntegerValue(NSDecimal decimalNumber)
 {
-	return (NSInteger)[[NSDecimalNumber decimalNumberWithDecimal:decimalNumber] intValue]; 
+	return (NSInteger)[[NSDecimalNumber decimalNumberWithDecimal:decimalNumber] integerValue]; 
+}
+
+/**
+ *	@brief Converts an NSDecimal value to an unsigned 8-bit integer.
+ *	@param decimalNumber The NSDecimal value.
+ *	@return The converted value.
+ **/
+uint8_t CPDecimalUnsignedCharValue(NSDecimal decimalNumber)
+{
+	return (uint8_t)[[NSDecimalNumber decimalNumberWithDecimal:decimalNumber] unsignedCharValue]; 
+}
+
+/**
+ *	@brief Converts an NSDecimal value to an unsigned 16-bit integer.
+ *	@param decimalNumber The NSDecimal value.
+ *	@return The converted value.
+ **/
+uint16_t CPDecimalUnsignedShortValue(NSDecimal decimalNumber)
+{
+	return (uint16_t)[[NSDecimalNumber decimalNumberWithDecimal:decimalNumber] unsignedShortValue]; 
+}
+
+/**
+ *	@brief Converts an NSDecimal value to an unsigned 32-bit integer.
+ *	@param decimalNumber The NSDecimal value.
+ *	@return The converted value.
+ **/
+uint32_t CPDecimalUnsignedLongValue(NSDecimal decimalNumber)
+{
+	return (uint32_t)[[NSDecimalNumber decimalNumberWithDecimal:decimalNumber] unsignedLongValue]; 
+}
+
+/**
+ *	@brief Converts an NSDecimal value to an unsigned 64-bit integer.
+ *	@param decimalNumber The NSDecimal value.
+ *	@return The converted value.
+ **/
+uint64_t CPDecimalUnsignedLongLongValue(NSDecimal decimalNumber)
+{
+	return (uint64_t)[[NSDecimalNumber decimalNumberWithDecimal:decimalNumber] unsignedLongLongValue]; 
+}
+
+/**
+ *	@brief Converts an NSDecimal value to an unsigned int.
+ *	@param decimalNumber The NSDecimal value.
+ *	@return The converted value.
+ **/
+unsigned int CPDecimalUnsignedIntValue(NSDecimal decimalNumber)
+{
+	return (unsigned int)[[NSDecimalNumber decimalNumberWithDecimal:decimalNumber] unsignedIntValue]; 
+}
+
+/**
+ *	@brief Converts an NSDecimal value to an NSUInteger.
+ *	@param decimalNumber The NSDecimal value.
+ *	@return The converted value.
+ **/
+NSUInteger CPDecimalUnsignedIntegerValue(NSDecimal decimalNumber)
+{
+	return (NSUInteger)[[NSDecimalNumber decimalNumberWithDecimal:decimalNumber] unsignedIntegerValue]; 
 }
 
 /**
@@ -35,7 +143,70 @@ double CPDecimalDoubleValue(NSDecimal decimalNumber)
 }
 
 /**
- *	@brief Converts a NSInteger value to an NSDecimal.
+ *	@brief Converts an NSDecimal value to an NSString.
+ *	@param decimalNumber The NSDecimal value.
+ *	@return The converted value.
+ **/
+NSString *CPDecimalStringValue(NSDecimal decimalNumber)
+{
+	return (NSString *)[[NSDecimalNumber decimalNumberWithDecimal:decimalNumber] stringValue]; 
+}
+
+#pragma mark -
+#pragma mark Convert primitive types to NSDecimal
+
+/**
+ *	@brief Converts an 8-bit integer value to an NSDecimal.
+ *	@param i The integer value.
+ *	@return The converted value.
+ **/
+NSDecimal CPDecimalFromChar(int8_t i)
+{
+	return [[NSNumber numberWithChar:i] decimalValue]; 
+}
+
+/**
+ *	@brief Converts a 16-bit integer value to an NSDecimal.
+ *	@param i The integer value.
+ *	@return The converted value.
+ **/
+NSDecimal CPDecimalFromShort(int16_t i)
+{
+	return [[NSNumber numberWithShort:i] decimalValue]; 
+}
+
+/**
+ *	@brief Converts a 32-bit integer value to an NSDecimal.
+ *	@param i The integer value.
+ *	@return The converted value.
+ **/
+NSDecimal CPDecimalFromLong(int32_t i)
+{
+	return [[NSNumber numberWithLong:i] decimalValue]; 
+}
+
+/**
+ *	@brief Converts a 64-bit integer value to an NSDecimal.
+ *	@param i The integer value.
+ *	@return The converted value.
+ **/
+NSDecimal CPDecimalFromLongLong(int64_t i)
+{
+	return [[NSNumber numberWithLongLong:i] decimalValue]; 
+}
+
+/**
+ *	@brief Converts an int value to an NSDecimal.
+ *	@param i The int value.
+ *	@return The converted value.
+ **/
+NSDecimal CPDecimalFromInt(int i)
+{
+	return [[NSNumber numberWithInt:i] decimalValue]; 
+}
+
+/**
+ *	@brief Converts an NSInteger value to an NSDecimal.
  *	@param i The NSInteger value.
  *	@return The converted value.
  **/
@@ -45,7 +216,57 @@ NSDecimal CPDecimalFromInteger(NSInteger i)
 }
 
 /**
- *	@brief Converts a NSUInteger value to an NSDecimal.
+ *	@brief Converts an unsigned 8-bit integer value to an NSDecimal.
+ *	@param i The unsigned integer value.
+ *	@return The converted value.
+ **/
+NSDecimal CPDecimalFromUnsignedChar(uint8_t i)
+{
+	return [[NSNumber numberWithUnsignedChar:i] decimalValue]; 
+}
+
+/**
+ *	@brief Converts an unsigned 16-bit integer value to an NSDecimal.
+ *	@param i The unsigned integer value.
+ *	@return The converted value.
+ **/
+NSDecimal CPDecimalFromUnsignedShort(uint16_t i)
+{
+	return [[NSNumber numberWithUnsignedShort:i] decimalValue]; 
+}
+
+/**
+ *	@brief Converts an unsigned 32-bit integer value to an NSDecimal.
+ *	@param i The unsigned integer value.
+ *	@return The converted value.
+ **/
+NSDecimal CPDecimalFromUnsignedLong(uint32_t i)
+{
+	return [[NSNumber numberWithUnsignedLong:i] decimalValue]; 
+}
+
+/**
+ *	@brief Converts an unsigned 64-bit integer value to an NSDecimal.
+ *	@param i The unsigned integer value.
+ *	@return The converted value.
+ **/
+NSDecimal CPDecimalFromUnsignedLongLong(uint64_t i)
+{
+	return [[NSNumber numberWithUnsignedLongLong:i] decimalValue]; 
+}
+
+/**
+ *	@brief Converts an unsigned int value to an NSDecimal.
+ *	@param i The unsigned int value.
+ *	@return The converted value.
+ **/
+NSDecimal CPDecimalFromUnsignedInt(unsigned int i)
+{
+	return [[NSNumber numberWithUnsignedInt:i] decimalValue]; 
+}
+
+/**
+ *	@brief Converts an NSUInteger value to an NSDecimal.
  *	@param i The NSUInteger value.
  *	@return The converted value.
  **/
@@ -73,6 +294,31 @@ NSDecimal CPDecimalFromDouble(double d)
 {
 	return [[NSNumber numberWithDouble:d] decimalValue]; 
 }
+
+/**
+ *	@brief Parses a string and extracts the numeric value as an NSDecimal.
+ *	@param stringRepresentation The string value.
+ *	@return The numeric value extracted from the string.
+ **/
+NSDecimal CPDecimalFromString(NSString *stringRepresentation)
+{
+	// The following NSDecimalNumber-based creation of NSDecimals from strings is slower than 
+	// the NSScanner-based method: (307000 operations per second vs. 582000 operations per second for NSScanner)
+	
+	/*	NSDecimalNumber *newNumber = [[NSDecimalNumber alloc] initWithString:@"1.0" locale:[NSLocale currentLocale]];
+	 newDecimal = [newNumber decimalValue];
+	 [newNumber release];*/
+	
+	NSDecimal result;
+	NSScanner *theScanner = [[NSScanner alloc] initWithString:stringRepresentation];
+	[theScanner scanDecimal:&result];
+	[theScanner release];
+	
+	return result;
+}
+
+#pragma mark -
+#pragma mark NSDecimal arithmetic
 
 /**
  *	@brief Adds two NSDecimals together.
@@ -125,6 +371,9 @@ NSDecimal CPDecimalDivide(NSDecimal numerator, NSDecimal denominator)
 	NSDecimalDivide(&result, &numerator, &denominator, NSRoundBankers);
 	return result;
 }
+
+#pragma mark -
+#pragma mark NSDecimal comparison
 
 /**
  *	@brief Checks to see if one NSDecimal is greater than another.
@@ -181,27 +430,8 @@ BOOL CPDecimalEquals(NSDecimal leftOperand, NSDecimal rightOperand)
 	return (NSDecimalCompare(&leftOperand, &rightOperand) == NSOrderedSame);	
 }
 
-/**
- *	@brief Parses a string and extracts the numeric value as an NSDecimal.
- *	@param stringRepresentation The string value.
- *	@return The numeric value extracted from the string.
- **/
-NSDecimal CPDecimalFromString(NSString *stringRepresentation)
-{
-	// The following NSDecimalNumber-based creation of NSDecimals from strings is slower than 
-	// the NSScanner-based method: (307000 operations per second vs. 582000 operations per second for NSScanner)
-	
-/*	NSDecimalNumber *newNumber = [[NSDecimalNumber alloc] initWithString:@"1.0" locale:[NSLocale currentLocale]];
-	newDecimal = [newNumber decimalValue];
-	[newNumber release];*/
-	
-	NSDecimal result;
-	NSScanner *theScanner = [[NSScanner alloc] initWithString:stringRepresentation];
-	[theScanner scanDecimal:&result];
-	[theScanner release];
-	
-	return result;
-}
+#pragma mark -
+#pragma mark NSDecimal utilities
 
 /**
  *	@brief Creates and returns an NSDecimal struct that represents the value "not a number".
