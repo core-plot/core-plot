0adb28f0fbb978f88acdae657368a2256ee95cdc
Eric Skroch <eskroch@mac.com>
2010-11-10 19:23:29 -0500

    Fixed the SDK path in the packaging script.

--- packaging/prepare.sh
+++ packaging/prepare.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-mkdir -p Library/SDKs
+mkdir -p Library/SDKs/CorePlotSDK
 mkdir -p Library/Frameworks
 mkdir -p Library/Developer/Shared/Documentation/DocSets
 
 mkdir -p CorePlot/Binaries/iOS/CorePlot
 unzip CorePlot/Binaries/iOS/CorePlot.zip -d CorePlot/Binaries/iOS/CorePlot
-cp -r CorePlot/Binaries/iOS/CorePlot/CorePlotSDK/* Library/SDKs
+cp -r CorePlot/Binaries/iOS/CorePlot/CorePlotSDK/* Library/SDKs/CorePlotSDK
 rm -r CorePlot/Binaries/iOS/CorePlot
 
 cp -r CorePlot/Binaries/MacOS/CorePlot.framework Library/Frameworks
