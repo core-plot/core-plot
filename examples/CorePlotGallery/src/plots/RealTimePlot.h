625f263ffe2a0a70156a19d21139ba963c2ad3cb
Eric Skroch <eskroch@mac.com>
2011-07-09 13:42:05 -0400

    Added a real-time plot sample to the Plot Gallery app.

---
+++ examples/CorePlotGallery/src/plots/RealTimePlot.h
@@ -0,0 +1,17 @@
+//
+//  RealTimePlot.h
+//  CorePlotGallery
+//
+
+#import "PlotItem.h"
+
+@interface RealTimePlot : PlotItem < CPTPlotDataSource >
+{
+    NSMutableArray* plotData;
+	NSUInteger currentIndex;
+	NSTimer *dataTimer;
+}
+
+-(void)newData:(NSTimer *)theTimer;
+
+@end
