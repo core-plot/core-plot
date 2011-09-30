207c9854a0f284c83dbe00adb7734934cfbd38b6
Eric Skroch <eskroch@mac.com>
2011-09-29 23:24:59 -0400

    Added automatic code formatting script and instructions.

---
+++ scripts/format_core_plot.sh
@@ -0,0 +1,17 @@
+#!/bin/sh 
+
+file_list=`find ".." -name "*.[mh]" \! -iregex ".*/build/.*" -type f | sed "s| \([^/]\)|:\1|g"`
+
+for file in $file_list
+do
+
+file2indent=`echo $file | sed "s|:| |g"`
+echo "Indenting file '$file2indent'"
+#!/bin/bash
+"/usr/local/bin/uncrustify" -l OC -f "$file2indent" -c "./uncrustify.cfg" -o "./indentoutput.tmp"
+
+# remove spaces before category names to keep Doxygen happy
+cat "./indentoutput.tmp" | sed "s|\(@interface .*\) \((.*)\)|\1\2|g" | sed "s|\(@implementation .*\) \((.*)\)|\1\2|g" > "$file2indent"
+rm "./indentoutput.tmp"
+
+done
