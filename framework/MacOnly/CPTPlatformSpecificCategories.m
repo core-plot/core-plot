c8ec22a52eb8f09292a5ceeee682ef1974c8d157
Eric Skroch <eskroch@mac.com>
2012-09-05 19:55:29 -0400

    Minor code formatting.

--- framework/MacOnly/CPTPlatformSpecificCategories.m
+++ framework/MacOnly/CPTPlatformSpecificCategories.m
@@ -12,7 +12,16 @@
 {
     CGSize boundsSize = self.bounds.size;
 
-    NSBitmapImageRep *layerImage     = [[NSBitmapImageRep alloc] initWithBitmapDataPlanes:NULL pixelsWide:boundsSize.width pixelsHigh:boundsSize.height bitsPerSample:8 samplesPerPixel:4 hasAlpha:YES isPlanar:NO colorSpaceName:NSCalibratedRGBColorSpace bytesPerRow:(NSInteger)boundsSize.width * 4 bitsPerPixel:32];
+    NSBitmapImageRep *layerImage = [[NSBitmapImageRep alloc] initWithBitmapDataPlanes:NULL
+                                                                           pixelsWide:boundsSize.width
+                                                                           pixelsHigh:boundsSize.height
+                                                                        bitsPerSample:8
+                                                                      samplesPerPixel:4
+                                                                             hasAlpha:YES
+                                                                             isPlanar:NO
+                                                                       colorSpaceName:NSCalibratedRGBColorSpace
+                                                                          bytesPerRow:(NSInteger)boundsSize.width * 4 bitsPerPixel:32];
+
     NSGraphicsContext *bitmapContext = [NSGraphicsContext graphicsContextWithBitmapImageRep:layerImage];
     CGContextRef context             = (CGContextRef)[bitmapContext graphicsPort];
 
