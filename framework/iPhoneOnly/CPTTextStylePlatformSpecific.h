40e24d4f6136060fc17eb2f48859a17ac97b0f0c
Eric Skroch <eskroch@mac.com>
2013-10-26 19:37:37 -0400

    Fixed a compile error when using an iOS SDK prior to iOS 7. Fixed issue 603.

--- framework/iPhoneOnly/CPTTextStylePlatformSpecific.h
+++ framework/iPhoneOnly/CPTTextStylePlatformSpecific.h
@@ -11,3 +11,18 @@ typedef enum  _CPTTextAlignment {
     CPTTextAlignmentNatural   = NSTextAlignmentNatural    ///< Natural alignment of the text's script.
 }
 CPTTextAlignment;
+
+// @cond
+// for iOS SDK compatibility
+#if TARGET_IPHONE_SIMULATOR || TARGET_OS_IPHONE
+#if __IPHONE_OS_VERSION_MAX_ALLOWED < 70000
+@interface NSString(CPTTextStylePlatformSpecificExtensions)
+
+-(CGSize)sizeWithAttributes:(NSDictionary *)attrs;
+
+@end
+#else
+#endif
+#endif
+
+/// @endcond
