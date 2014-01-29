46a6ff5a705b5e7792877da34bf98241f714b81f
Eric Skroch <eskroch@mac.com>
2014-01-29 16:19:19 -0500

    Use the CGPathCreateWithRoundedRect() function when available to improve drawing performance.

--- framework/Source/CPTPathExtensions.m
+++ framework/Source/CPTPathExtensions.m
@@ -10,30 +10,38 @@
  **/
 CGPathRef CreateRoundedRectPath(CGRect rect, CGFloat cornerRadius)
 {
-    // In order to draw a rounded rectangle, we will take advantage of the fact that
-    // CGPathAddArcToPoint will draw straight lines past the start and end of the arc
-    // in order to create the path from the current position and the destination position.
-
-    CGFloat minX = CGRectGetMinX(rect), midX = CGRectGetMidX(rect), maxX = CGRectGetMaxX(rect);
-    CGFloat minY = CGRectGetMinY(rect), midY = CGRectGetMidY(rect), maxY = CGRectGetMaxY(rect);
-
-    CGMutablePathRef path = CGPathCreateMutable();
-
     if ( cornerRadius > CPTFloat(0.0) ) {
         cornerRadius = MIN( MIN( cornerRadius, rect.size.width * CPTFloat(0.5) ), rect.size.height * CPTFloat(0.5) );
 
-        CGPathMoveToPoint(path, NULL, minX, midY);
-        CGPathAddArcToPoint(path, NULL, minX, minY, midX, minY, cornerRadius);
-        CGPathAddArcToPoint(path, NULL, maxX, minY, maxX, midY, cornerRadius);
-        CGPathAddArcToPoint(path, NULL, maxX, maxY, midX, maxY, cornerRadius);
-        CGPathAddArcToPoint(path, NULL, minX, maxY, minX, midY, cornerRadius);
-        CGPathCloseSubpath(path);
+        // CGPathCreateWithRoundedRect() is available in Mac OS X 10.9 and iOS 7 but not marked in the header file
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunreachable-code"
+        if ( CGPathCreateWithRoundedRect ) {
+            return CGPathCreateWithRoundedRect(rect, cornerRadius, cornerRadius, NULL);
+        }
+        else {
+            // In order to draw a rounded rectangle, we will take advantage of the fact that
+            // CGPathAddArcToPoint will draw straight lines past the start and end of the arc
+            // in order to create the path from the current position and the destination position.
+            CGFloat minX = CGRectGetMinX(rect), midX = CGRectGetMidX(rect), maxX = CGRectGetMaxX(rect);
+            CGFloat minY = CGRectGetMinY(rect), midY = CGRectGetMidY(rect), maxY = CGRectGetMaxY(rect);
+
+            CGMutablePathRef path = CGPathCreateMutable();
+
+            CGPathMoveToPoint(path, NULL, minX, midY);
+            CGPathAddArcToPoint(path, NULL, minX, minY, midX, minY, cornerRadius);
+            CGPathAddArcToPoint(path, NULL, maxX, minY, maxX, midY, cornerRadius);
+            CGPathAddArcToPoint(path, NULL, maxX, maxY, midX, maxY, cornerRadius);
+            CGPathAddArcToPoint(path, NULL, minX, maxY, minX, midY, cornerRadius);
+            CGPathCloseSubpath(path);
+
+            return path;
+        }
+#pragma clang diagnostic pop
     }
     else {
-        CGPathAddRect(path, NULL, rect);
+        return CGPathCreateWithRect(rect, NULL);
     }
-
-    return path;
 }
 
 /** @brief Adds a rectangular path with rounded corners to a graphics context.
