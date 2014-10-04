c00a9eb902bfce230c6f6253feb826eacc4ca517
Eric Skroch <eskroch@mac.com>
2014-10-03 20:11:39 -0400

    Turned on the standard Core Plot warnings in the DatePlot example app and fixed the resulting warnings.

--- examples/DatePlot/Controller.m
+++ examples/DatePlot/Controller.m
@@ -1,7 +1,21 @@
 #import "Controller.h"
 #import <CorePlot/CorePlot.h>
 
+@interface Controller()
+
+@property (nonatomic, readwrite, strong) IBOutlet CPTGraphHostingView *hostView;
+@property (nonatomic, readwrite, strong) CPTXYGraph *graph;
+@property (nonatomic, readwrite, strong) NSArray *plotData;
+
+@end
+
+#pragma mark -
+
 @implementation Controller
+
+@synthesize hostView;
+@synthesize graph;
+@synthesize plotData;
 
 -(void)awakeFromNib
 {
@@ -15,21 +29,23 @@
     NSTimeInterval oneDay      = 24 * 60 * 60;
 
     // Create graph from theme
-    graph = [[CPTXYGraph alloc] initWithFrame:CGRectZero];
-    CPTTheme *theme = [CPTTheme themeNamed:kCPTDarkGradientTheme];
-    [graph applyTheme:theme];
-    hostView.hostedGraph = graph;
+    CPTXYGraph *newGraph = [[CPTXYGraph alloc] initWithFrame:CGRectZero];
+    CPTTheme *theme      = [CPTTheme themeNamed:kCPTDarkGradientTheme];
+    [newGraph applyTheme:theme];
+    self.graph = newGraph;
+
+    self.hostView.hostedGraph = newGraph;
 
     // Setup scatter plot space
-    CPTXYPlotSpace *plotSpace = (CPTXYPlotSpace *)graph.defaultPlotSpace;
+    CPTXYPlotSpace *plotSpace = (CPTXYPlotSpace *)newGraph.defaultPlotSpace;
     NSTimeInterval xLow       = 0.0;
     plotSpace.xRange = [CPTPlotRange plotRangeWithLocation:CPTDecimalFromDouble(xLow) length:CPTDecimalFromDouble(oneDay * 5.0)];
     plotSpace.yRange = [CPTPlotRange plotRangeWithLocation:CPTDecimalFromDouble(1.0) length:CPTDecimalFromDouble(3.0)];
 
     // Axes
-    CPTXYAxisSet *axisSet = (CPTXYAxisSet *)graph.axisSet;
+    CPTXYAxisSet *axisSet = (CPTXYAxisSet *)newGraph.axisSet;
     CPTXYAxis *x          = axisSet.xAxis;
-    x.majorIntervalLength         = CPTDecimalFromFloat(oneDay);
+    x.majorIntervalLength         = CPTDecimalFromDouble(oneDay);
     x.orthogonalCoordinateDecimal = CPTDecimalFromDouble(2.0);
     x.minorTicksPerInterval       = 0;
     NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
@@ -41,7 +57,7 @@
     CPTXYAxis *y = axisSet.yAxis;
     y.majorIntervalLength         = CPTDecimalFromDouble(0.5);
     y.minorTicksPerInterval       = 5;
-    y.orthogonalCoordinateDecimal = CPTDecimalFromFloat(oneDay);
+    y.orthogonalCoordinateDecimal = CPTDecimalFromDouble(oneDay);
 
     // Create a plot that uses the data source method
     CPTScatterPlot *dataSourceLinePlot = [[CPTScatterPlot alloc] init];
@@ -53,19 +69,19 @@
     dataSourceLinePlot.dataLineStyle = lineStyle;
 
     dataSourceLinePlot.dataSource = self;
-    [graph addPlot:dataSourceLinePlot];
+    [newGraph addPlot:dataSourceLinePlot];
 
     // Add some data
     NSMutableArray *newData = [NSMutableArray array];
     for ( NSUInteger i = 0; i < 5; i++ ) {
-        NSTimeInterval x = oneDay * i;
-        NSNumber *y      = @(1.2 * rand() / (double)RAND_MAX + 1.2);
+        NSTimeInterval xVal = oneDay * i;
+        double yVal         = 1.2 * rand() / (double)RAND_MAX + 1.2;
         [newData addObject:
-         @{ @(CPTScatterPlotFieldX): @(x),
-            @(CPTScatterPlotFieldY): y }
+         @{ @(CPTScatterPlotFieldX): @(xVal),
+            @(CPTScatterPlotFieldY): @(yVal) }
         ];
     }
-    plotData = newData;
+    self.plotData = newData;
 }
 
 #pragma mark -
@@ -73,12 +89,12 @@
 
 -(NSUInteger)numberOfRecordsForPlot:(CPTPlot *)plot
 {
-    return plotData.count;
+    return self.plotData.count;
 }
 
 -(id)numberForPlot:(CPTPlot *)plot field:(NSUInteger)fieldEnum recordIndex:(NSUInteger)index
 {
-    return plotData[index][@(fieldEnum)];
+    return self.plotData[index][@(fieldEnum)];
 }
 
 @end
