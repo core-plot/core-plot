99f54fe6bec9e6830f44e3a41547cd62739116ca
Eric Skroch <eskroch@mac.com>
2014-11-16 17:59:38 -0500

    Update comments with modern Objective-C syntax.

--- examples/StockPlot/Classes/RootViewController.m
+++ examples/StockPlot/Classes/RootViewController.m
@@ -294,7 +294,7 @@
  *
  * if (editingStyle == UITableViewCellEditingStyleDelete) {
  * // Delete the row from the data source.
- * [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
+ * [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
  * }
  * else if (editingStyle == UITableViewCellEditingStyleInsert) {
  * // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
