3413d8b7fa382921a918e7801a22e614a17b8bbf
IngmarStein <IngmarStein@users.noreply.github.com>
2014-12-31 09:39:45 +0100

    Fix compilation when ENABLE_STRICT_OBJC_MSGSEND is set (enabled by default in CocoaPods 0.36.0)

--- framework/Source/_CPTAnimationPlotRangePeriod.m
+++ framework/Source/_CPTAnimationPlotRangePeriod.m
@@ -7,14 +7,16 @@
 
 -(void)setStartValueFromObject:(id)boundObject propertyGetter:(SEL)boundGetter
 {
-    IMP getterMethod = [boundObject methodForSelector:boundGetter];
+    typedef NSValue *(*GetterType)(id, SEL);
+    GetterType getterMethod = (GetterType)[boundObject methodForSelector:boundGetter];
 
     self.startValue = getterMethod(boundObject, boundGetter);
 }
 
 -(BOOL)canStartWithValueFromObject:(id)boundObject propertyGetter:(SEL)boundGetter
 {
-    IMP getterMethod = [boundObject methodForSelector:boundGetter];
+    typedef CPTPlotRange *(*GetterType)(id, SEL);
+    GetterType getterMethod = (GetterType)[boundObject methodForSelector:boundGetter];
 
     CPTPlotRange *current = getterMethod(boundObject, boundGetter);
     CPTPlotRange *start   = (CPTPlotRange *)self.startValue;
