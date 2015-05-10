777d948505b0c437b9869fde1601944c79e6ed02
Eric Skroch <eskroch@mac.com>
2015-05-09 21:48:46 -0400

    Undo previous commit.

--- framework/Source/CPTPlot.m
+++ framework/Source/CPTPlot.m
@@ -549,11 +549,9 @@ NSString *const CPTPlotBindingDataLabels = @"dataLabels"; ///< Plot data labels.
  **/
 -(void)reloadDataInIndexRange:(NSRange)indexRange
 {
-#if debug
     id<CPTPlotDataSource> theDataSource = (id<CPTPlotDataSource>)self.dataSource;
 
     NSParameterAssert(NSMaxRange(indexRange) <= [theDataSource numberOfRecordsForPlot:self]);
-#endif
 
     self.dataNeedsReloading = NO;
 
