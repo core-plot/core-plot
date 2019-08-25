e7c0b4e19df6042c94430efcf4dda4f3ea1613b5
Eric Skroch <eskroch@mac.com>
2019-08-25 14:53:34 -0400

    Use CPTFloat() literal value instead of a variable to satisfy CGFloat type checking.

--- framework/Source/CPTLegend.m
+++ framework/Source/CPTLegend.m
@@ -581,13 +581,12 @@ CPTLegendNotification const CPTLegendNeedsReloadEntriesForPlotNotification = @"C
     CGFloat padRight  = self.entryPaddingRight;
     CGFloat padBottom = self.entryPaddingBottom;
 
-    CGFloat zero = 0.0;
     for ( NSUInteger col = 0; col < columnCount; col++ ) {
         NSNumber *colWidth = computedColumnWidths[col];
         CGFloat width      = [colWidth cgFloatValue];
         actualColumnWidths[col] = width;
         if ( col < columnCount - 1 ) {
-            columnPositions[col + 1] = columnPositions[col] + padLeft + width + padRight + (isHorizontalLayout ? theOffset + theSwatchSize.width : zero) + theColumnMargin;
+            columnPositions[col + 1] = columnPositions[col] + padLeft + width + padRight + (isHorizontalLayout ? theOffset + theSwatchSize.width : CPTFloat(0.0)) + theColumnMargin;
         }
     }
 
@@ -606,7 +605,7 @@ CPTLegendNotification const CPTLegendNeedsReloadEntriesForPlotNotification = @"C
         CGFloat height      = [rowHeight cgFloatValue];
         actualRowHeights[row] = height;
         if ( row < rowCount - 1 ) {
-            rowPositions[row] = rowPositions[row + 1] + padBottom + lastRowHeight + padTop + (isHorizontalLayout ? zero : theOffset + theSwatchSize.height) + theRowMargin;
+            rowPositions[row] = rowPositions[row + 1] + padBottom + lastRowHeight + padTop + (isHorizontalLayout ? CPTFloat(0.0) : theOffset + theSwatchSize.height) + theRowMargin;
         }
         lastRowHeight = height;
     }
