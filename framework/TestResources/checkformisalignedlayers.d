a3a46a633d9cec3da037570c894d3b4d7c1dafa7
Eric Skroch <eskroch@mac.com>
2020-03-01 13:01:07 -0500

    Removed Dtrace probe source file.

--- framework/TestResources/checkformisalignedlayers.d
+++
@@ -1,6 +0,0 @@
-#pragma D option quiet
-
-CorePlot$target:::layer_position_change
-{
-	printf("Misaligned layer: %20s (%u.%03u, %u.%03u, %u.%03u, %u.%03u)\n", copyinstr(arg0), arg1 / 1000, arg1 % 1000, arg2 / 1000, arg2 % 1000, arg3 / 1000, arg3 % 1000, arg4 / 1000, arg4 % 1000 );
-}
