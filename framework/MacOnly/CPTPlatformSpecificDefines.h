ecdb911575bda19aef559786dc4a66eb9a74d8bd
3a4oT <petro.rovenskyy@gmail.com>
2020-12-12 00:05:22 +0200

    moved xcode project settings to PlatformSpecific layout. Xcode tests passed.

--- framework/MacOnly/CPTPlatformSpecificDefines.h
+++
@@ -1,6 +0,0 @@
-/// @file
-
-typedef NSColor CPTNativeColor; ///< Platform-native color.
-typedef NSImage CPTNativeImage; ///< Platform-native image format.
-typedef NSEvent CPTNativeEvent; ///< Platform-native OS event.
-typedef NSFont  CPTNativeFont;  ///< Platform-native font.
