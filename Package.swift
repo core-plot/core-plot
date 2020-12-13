e4480fed6aaf63f88dfadb99174a47743eb2f014
3a4oT <petro.rovenskyy@gmail.com>
2020-12-14 01:14:23 +0200

    fixed package name so you can @import CorePlot instead of `@import core-plot`

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "core-plot",
+    name: "CorePlot",
     platforms: [
               .macOS(.v10_12),
               .iOS(.v10),
@@ -13,8 +13,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "core-plot",
-            targets: ["core-plot"]),
+            name: "CorePlot",
+            targets: ["CorePlot"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -24,7 +24,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "core-plot",
+            name: "CorePlot",
             dependencies: [],
             path: "spm/Sources/core-plot"),
     ]
