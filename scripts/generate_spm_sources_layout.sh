1e6afadef586c8c5d9c8db64e287f0bb81fe5c1b
Eric Skroch <eskroch@mac.com>
2021-02-06 12:12:17 -0500

    Added quotes in the Swift Package Manager script to accommodate spaces in file paths.

--- scripts/generate_spm_sources_layout.sh
+++ scripts/generate_spm_sources_layout.sh
@@ -2,18 +2,18 @@
 
 set -e
 
-SPM_PUBLIC_HEADERS_PATH="spm/Sources/core-plot/include"
 SPM_SOURCES_PATH="spm/Sources/core-plot"
+SPM_PUBLIC_HEADERS_PATH="$SPM_SOURCES_PATH/include"
 
 # Delete all symbolik links from `spm` folder
 function cleanup() {
-    rm -rf $SPM_PUBLIC_HEADERS_PATH/*.[hm]
-    rm -rf $SPM_SOURCES_PATH/*.[hm]
+    rm -rf "$SPM_PUBLIC_HEADERS_PATH"/*.[hm]
+    rm -rf "$SPM_SOURCES_PATH"/*.[hm]
 }
 
 function generate_spm_public_headers() {
     echo "Generate symbolic links for all public headers. *.h"
-    echo "Generated under ./spm/Sources/core-plot/include"
+    echo "Generated under $SPM_PUBLIC_HEADERS_PATH"
 
     public_headers_list=$(
         find "framework" -name "*.[h]" \
@@ -25,37 +25,42 @@ function generate_spm_public_headers() {
             -not -path "framework/CorePlot.h" | sed "s| \([^/]\)|:\1|g"
     )
 
-    SRC_ROOT=$(pwd)
-    cd $SPM_PUBLIC_HEADERS_PATH
+    SRC_ROOT="$(pwd)"
+
+    cd "$SPM_PUBLIC_HEADERS_PATH"
+
     for public_file in $public_headers_list; do
-        file_to_link=$(echo $public_file | sed "s|:| |g")
-        ln -s ../../../../$file_to_link
+        file_to_link=$(echo "$public_file" | sed "s|:| |g")
+        ln -s "../../../../$file_to_link"
 
     done
 
-    cd $SRC_ROOT
+    cd "$SRC_ROOT"
+
     echo "      Done"
     echo ""
 }
 
 function generate_spm_private_sources() {
     echo "Generate symbolic links for all private headers/implementations. _*.h && _*.m"
-    echo "Generated under ./spm/Sources/core-plot"
+    echo "Generated under $SPM_SOURCES_PATH"
 
     private_sources_list=$(find "framework" \
         -name "_*.[mh]" \
         -type f | sed "s| \([^/]\)|:\1|g")
 
-    SRC_ROOT=$(pwd)
-    cd $SPM_SOURCES_PATH
-    for private_file in $private_sources_list; do
-        file_to_link=$(echo $private_file | sed "s|:| |g")
+    SRC_ROOT="$(pwd)"
 
-        ln -s ../../../$file_to_link
+    cd "$SPM_SOURCES_PATH"
+
+    for private_file in $private_sources_list; do
+        file_to_link=$(echo "$private_file" | sed "s|:| |g")
+
+        ln -s "../../../$file_to_link"
 
     done
 
-    cd $SRC_ROOT
+    cd "$SRC_ROOT"
 
     echo "      Done"
     echo ""
@@ -63,23 +68,24 @@ function generate_spm_private_sources() {
 
 function generate_spm_public_sources() {
     echo "Generate symbolic links for all public implementations. *.m"
-    echo "Generated under ./spm/Sources/core-plot"
+    echo "Generated under $SPM_SOURCES_PATH"
 
     public_sources_list=$(find "framework" -name "*.[m]" \
         \! -name "*Test*.[hm]" \
         \! -name "_*.[hm]" \
         -type f -not -path "*/MacOnly/*" | sed "s| \([^/]\)|:\1|g")
 
-    SRC_ROOT=$(pwd)
-    cd $SPM_SOURCES_PATH
+    SRC_ROOT="$(pwd)"
+
+    cd "$SPM_SOURCES_PATH"
 
     for public_file in $public_sources_list; do
-        file_to_link=$(echo $public_file | sed "s|:| |g")
-        ln -s ../../../$file_to_link
+        file_to_link=$(echo "$public_file" | sed "s|:| |g")
+        ln -s "../../../$file_to_link"
 
     done
 
-    cd $SRC_ROOT
+    cd "$SRC_ROOT"
 
     echo "      Done"
     echo ""
