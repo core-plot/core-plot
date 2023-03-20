1ef582f60feee69525af2517fb82d74689839267
Eric Skroch <eskroch@mac.com>
2023-03-19 21:31:56 -0400

    Updated the deployment targets in the Package.swift file to match the requirements defined in the project.

--- Package.swift
+++ Package.swift
@@ -6,9 +6,9 @@ import PackageDescription
 let package = Package(
     name: "CorePlot",
     platforms: [
-              .macOS(.v10_12),
-              .iOS(.v10),
-              .tvOS(.v10)
+              .macOS(.v10_14),
+              .iOS(.v12),
+              .tvOS(.v12)
           ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
